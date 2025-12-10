e0af0d75d9f19be557995ca4c103d3cca2a5e427
Teenu2005 <teenuanandanand@gmail.com>
2025-12-10 16:18:16 +0530

    Enterprice_IntialStage

--- AGGrid/src/main.jsx
+++ AGGrid/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 //AG grid moduler injuction
+// import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+// ModuleRegistry.registerModules([AllCommunityModule]);
 import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
-ModuleRegistry.registerModules([AllCommunityModule]);
+import { AllEnterpriseModule } from 'ag-grid-enterprise';
 
+ModuleRegistry.registerModules([AllEnterpriseModule]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
